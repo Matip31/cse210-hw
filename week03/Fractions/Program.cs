bf0156840fcdfb9a25ae053fa3e75db619565331
matip31 <matiasmiguel31@gmail.com>
2026-05-18 21:45:27 -0300

    learning activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f=new Fraction();
+        Console.WriteLine(f.GetFractionString());
+        Fraction fr=new Fraction(5);
+        Console.WriteLine(fr.GetFractionString());
+        Fraction fra=new Fraction(3,4);
+        Console.WriteLine(fra.GetFractionString());
+        Console.WriteLine(fra.GetDecimalValue());
     }
 }
